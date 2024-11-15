6f426f474d32e4d557954ab3c7f3114987ba02e9
Tzurikto <tzuriel.vera@anahuac.mx>
2024-11-15 13:59:40 -0600

    Update MAIN.py

--- PROYECTO_FINAL/MAIN.py
+++ PROYECTO_FINAL/MAIN.py
@@ -14,8 +14,9 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Cuerpos Celestes")
 running = True
 while running:
+    #HOLA
     for event in pygame.event.get():
-        
+
         if event.type == pygame.QUIT:
             running = False
 
